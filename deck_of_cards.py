344218a8057a751560b88ce01f8c0ec029576879
prithviBytes <prithvisuvarna56@gmail.com>
2021-03-15 20:34:37 +0530

    Add Deck Class

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,3 +1,4 @@
+from random import shuffle
 
 class Card:
 
@@ -9,3 +10,36 @@ class Card:
         self.value = value
         self.suit = suit
     
+class Deck:
+
+    def __init__(self):
+        suits = ["Hearts","Diamonds","Clubs","Spades"]
+        values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+        self.cards = [Card(value,suit) for value in values for suit in suits]
+
+    def __repr__(self):
+        return "Deck of {} cards".format(self.count())
+
+    def count(self):
+        return len(self.cards)
+
+    def _deal(self,num):
+        count = self.count()
+        actual = min(count,num)
+        if count == 0:
+            raise ValueError("All cards have been dealt")
+        cards = self.cards[-actual:]
+        self.cards = self.cards[:-actual]
+        return cards
+
+    def deal_card(self):
+        return self._deal(1)[0]
+
+    def deal_hand(self,hand_size):
+        return self._deal(hand_size)
+
+    def shuffle(self):
+        if self.count() < 52:
+            raise ValueError("Only full deck can be shuffled")
+        shuffle(self.cards)
+        return self
